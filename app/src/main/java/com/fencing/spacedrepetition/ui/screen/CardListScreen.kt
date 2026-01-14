d745a452a3d3e2d6b4c25545eb4412ba56ddfd97
Enmar Abrams <enmarabrams@gmail.com>
2026-01-13 19:52:22 -0500

    Fix import issues

--- app/src/main/java/com/fencing/spacedrepetition/ui/screen/CardListScreen.kt
+++ app/src/main/java/com/fencing/spacedrepetition/ui/screen/CardListScreen.kt
@@ -5,12 +5,15 @@ import androidx.activity.compose.BackHandler
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
 import androidx.compose.material3.*
